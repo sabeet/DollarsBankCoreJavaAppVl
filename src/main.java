db5134a0d99d9341c06ffbbc85727f678457e5fe
sabeet <sabeet.other.way@gmail.com>
2021-08-04 13:02:09 -0400

    "Added separate welcome package for bank"

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
+import menu.welcome;
+
 public class main {
     public static void main(String[] args) {
+        welcome.DollarWelcome();
         System.out.println("Let the games begin...");
     }
 }
---
+++ src/menu/welcome.java
@@ -0,0 +1,9 @@
+package menu;
+
+public class welcome {
+    public static void DollarWelcome() {
+        System.out.println( "+--------------------------+\n" +
+                            "| DOLLARSBANK Welcomes you |\n" +
+                            "+--------------------------+\n");
+    }
+}
